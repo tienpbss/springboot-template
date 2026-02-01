a73f643dece85757507ec45d2a4505b6480869cf
tienpbss <tienphung.cn07@gmail.com>
2026-02-01 17:39:38 +0700

    add catch valid exception

--- src/main/java/com/spring/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/spring/demo/exception/GlobalExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.spring.demo.exception;
 
 import com.spring.demo.entity.ApiResponse;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,5 +20,17 @@ public class GlobalExceptionHandler {
 
         ApiResponse<Void> response = ApiResponse.error(ex.getMessage());
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Void>> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+
+        List<String> errors = ex.getBindingResult()
+                .getAllErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .toList();
+        ApiResponse<Void> response = ApiResponse.error(String.join(", ", errors));
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
